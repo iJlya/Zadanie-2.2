0acade80552bf119ade800dc6a8997c58d7dd46a
*iLya* <*kruchkov.i.n@gmail.com*>
2023-10-16 16:25:17 +0300

    gitignore

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int score = 100; // начальный счет
+        int replenishment = 1100; // сумма пополнения
+
+        int bonus;
+        if (replenishment > 1000) {
+            bonus = replenishment / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println("bonus:" + bonus);
+
+    }
+}
